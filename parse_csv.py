3a303fe9711b3a2039334ea71be499cba5f0a240
ROOPE EEMELI PAIVANIEMI <r.paivaniemi.21@abdn.ac.uk>
2024-03-03 12:47:56 +0000

    Working

--- parse_csv.py
+++ parse_csv.py
@@ -74,5 +74,134 @@ with open('Database/Socio-Economic_Baseline_Data_Africa.csv', newline='') as f:
 
     cur.execute('INSERT INTO Africa VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
     conn.commit()
+
+with open('Database/Socio-Economic_Baseline_Data_Australasia.csv', newline='') as f:
+  reader = csv.reader(f, delimiter=",")
+  next(reader)
+  for row in reader:
+    print(row)
+
+    Country = row[0]
+    TotalPop = row[1]
+    PopDens = row[2]
+    GDP_Cap = row[6]
+    GDP_Growth = row[10]
+    LAND = row[11]
+
+    cur.execute('INSERT INTO Australasia VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
+    conn.commit()
+
+with open('Database/Socio-Economic_Baseline_Data_Europe.csv', newline='') as f:
+  reader = csv.reader(f, delimiter=",")
+  next(reader)
+  for row in reader:
+    print(row)
+
+    Country = row[0]
+    TotalPop = row[1]
+    PopDens = row[2]
+    GDP_Cap = row[6]
+    GDP_Growth = row[10]
+    LAND = row[11]
+
+    cur.execute('INSERT INTO Europe VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
+    conn.commit()
+
+with open('Database/Socio-Economic_Baseline_Data_LatinAmerica.csv', newline='') as f:
+  reader = csv.reader(f, delimiter=",")
+  next(reader)
+  for row in reader:
+    print(row)
+
+    Country = row[0]
+    TotalPop = row[1]
+    PopDens = row[2]
+    GDP_Cap = row[6]
+    GDP_Growth = row[10]
+    LAND = row[11]
+
+    cur.execute('INSERT INTO LatinAmerica VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
+    conn.commit()
+
+with open('Database/Socio-Economic_Baseline_Data_MiddleEast.csv', newline='') as f:
+  reader = csv.reader(f, delimiter=",")
+  next(reader)
+  for row in reader:
+    print(row)
+
+    Country = row[0]
+    TotalPop = row[1]
+    PopDens = row[2]
+    GDP_Cap = row[6]
+    GDP_Growth = row[10]
+    LAND = row[11]
+
+    cur.execute('INSERT INTO MiddleEast VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
+    conn.commit()
+  
+with open('Database/Socio-Economic_Baseline_Data_NorthAmerica.csv', newline='') as f:
+  reader = csv.reader(f, delimiter=",")
+  next(reader)
+  for row in reader:
+    print(row)
+
+    Country = row[0]
+    TotalPop = row[1]
+    PopDens = row[2]
+    GDP_Cap = row[6]
+    GDP_Growth = row[10]
+    LAND = row[11]
+
+    cur.execute('INSERT INTO NorthAmerica VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
+    conn.commit()
+
+with open('Database/Socio-Economic_Baseline_Data_SmallIslandStates.csv', newline='') as f:
+  reader = csv.reader(f, delimiter=",")
+  next(reader)
+  for row in reader:
+    print(row)
+
+    Country = row[0]
+    TotalPop = row[1]
+    PopDens = row[2]
+    GDP_Cap = row[6]
+    GDP_Growth = row[10]
+    LAND = row[11]
+
+    cur.execute('INSERT INTO SmallIslandStates VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
+    conn.commit()
+
+with open('Database/Socio-Economic_Baseline_Data_TemperateAsia.csv', newline='') as f:
+  reader = csv.reader(f, delimiter=",")
+  next(reader)
+  for row in reader:
+    print(row)
+
+    Country = row[0]
+    TotalPop = row[1]
+    PopDens = row[2]
+    GDP_Cap = row[6]
+    GDP_Growth = row[10]
+    LAND = row[11]
+
+    cur.execute('INSERT INTO TemperateAsia VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
+    conn.commit()
+
+with open('Database/Socio-Economic_Baseline_Data_TropicalAsia.csv', newline='') as f:
+  reader = csv.reader(f, delimiter=",")
+  next(reader)
+  for row in reader:
+    print(row)
+
+    Country = row[0]
+    TotalPop = row[1]
+    PopDens = row[2]
+    GDP_Cap = row[6]
+    GDP_Growth = row[10]
+    LAND = row[11]
+
+    cur.execute('INSERT INTO TropicalAsia VALUES (?,?,?,?,?,?)', (Country, TotalPop, PopDens, GDP_Cap, GDP_Growth, LAND))
+    conn.commit()
+
 print("Data parsed successfully");
 conn.close()
